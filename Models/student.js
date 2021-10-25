de49ea5ec94d7e534e68d43737dd811a2ed58168
Jerome <hamiltoj@roehampton.ac.uk>
2021-10-25 13:44:37 +0100

    updates

--- Models/student.js
+++ Models/student.js
@@ -9,6 +9,21 @@ class Student {
       this.lastName = lastName;
       this.courseId = courseId;
     }
+    getFormattedName() {
+      return this.#firstName + '' + this.#lastName;
+    }
+    get firstName() {
+      return this.#firstName;
+    }
+    get lastName() {
+      return this.#lastName;
+    }
+    set firstName(firstName) {
+      this.#firstName = firstName;
+    }
+    set lastName(lastName) {
+      this.#lastName = lastName;
+    }
   }
   module.exports =  {
     Student
